96ba6cf57703abd415dd843ec745d728044bdd30
kodurivamshi <vkoduri555@gmail.com>
2020-07-04 16:35:55 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('like').addEventListener("click",(event)=>{
+    document.getElementById("li").src="images/liked.jpg";
+})
+
+document.getElementById('like1').addEventListener("click",(event)=>{
+    document.getElementById("li1").src="images/liked.jpg";
+})
+
+
+document.getElementById('like2').addEventListener("click",(event)=>{
+    document.getElementById("li2").src="images/liked.jpg";
+})
+
+document.getElementById("comment").addEventListener("click",()=>{
+    var i=document.createElement("input")
+    i.setAttribute("type","text");
+    i.setAttribute("placeholder","comment");
+    i.setAttribute("id","c");
+    var b=document.createElement("input")
+    b.type="submit";
+    b.value="post";
+    b.setAttribute("onclick","add()");
+    var d=document.createElement("div");
+    d.appendChild(i);
+    d.appendChild(b);
+    document.getElementById("add").appendChild(d);
+})
+
+function add(){
+    var c=document.getElementById("c").value;
+    var p=document.createElement("p");
+    p.innerHTML=c;
+    var d=document.createElement("div").appendChild(p);
+    document.getElementById("com").appendChild(d);
+    document.getElementById("add").style.visibility="hidden";
+}
+
