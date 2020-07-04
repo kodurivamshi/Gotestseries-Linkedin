2f76f1e2f8074455f13552d51884e30202a0b5cb
kodurivamshi <vkoduri555@gmail.com>
2020-07-04 17:09:38 +0530

    thirdcommit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.getElementById('like').addEventListener("click",(event)=>{
-    document.getElementById("li").src="/images/liked.jpg";
+    document.getElementById("li").src="images/liked.JPG";
 })
 
 document.getElementById('like1').addEventListener("click",(event)=>{
     document.getElementById("li1").src="/images/liked.jpg";
 })
 
-
 document.getElementById('like2').addEventListener("click",(event)=>{
     document.getElementById("li2").src="/images/liked.jpg";
 })
+
 
 document.getElementById("comment").addEventListener("click",()=>{
     var i=document.createElement("input")
@@ -25,6 +25,7 @@ document.getElementById("comment").addEventListener("click",()=>{
     d.appendChild(b);
     document.getElementById("add").appendChild(d);
 })
+
 
 function add(){
     var c=document.getElementById("c").value;
